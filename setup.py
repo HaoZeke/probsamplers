c175a2b335431ba006c7611895ca52d9e1672314
Rohit Goswami <r95g10@gmail.com>
2026-07-06 15:19:59 -0500

    chore: release 0.1.1 (Windows-safe package layout)
    Ship the aux → _aux rename so source trees extract on Windows
    (reserved device name), and cut a tag for conda packaging.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='probsamplers',
-    version='0.1.0',
+    version='0.1.1',
     description='JupyterLab version of mcmc-demo',
     author='Rohit Goswami',
     author_email='rog32@hi.is',
